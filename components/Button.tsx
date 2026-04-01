186c5121f1cccdcf3909166cae5eb0070e26d838
komiljonovsardorbek80-cloud <komiljonovsardorbek80@gmail.com>
2026-04-01 22:57:36 +0500

    tuzatilgan

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,24 +1,33 @@
+import React from "react";
 
 type ButtonPropsType = {
-  title?: string
-  icon?: React.ReactNode
-  type:"button" | "submit"
-  iconPost?: "left" | "right"
-  extraStyle?: string
-  onClick?: React.MouseEventHandler<HTMLButtonElement>
-}
+  title?: string;
+  icon?: React.ReactNode;
+  type?: "button" | "submit";
+  iconPost?: "left" | "right";
+  extraStyle?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+};
 
-function Button({type,title, icon, iconPost, extraStyle,  onClick }: ButtonPropsType) {
+function Button({
+  type = "button",
+  title,
+  icon,
+  iconPost = "left",
+  extraStyle = "",
+  onClick,
+}: ButtonPropsType) {
   return (
     <button
-    type={type}
-    onClick={onClick}
-      className={`${extraStyle}   border-2  cursor-pointer border-black duration-300 active:opacity-20`}>
+      type={type}
+      onClick={onClick}
+      className={`${extraStyle} border-2 border-black cursor-pointer duration-300 active:opacity-20 flex items-center gap-2`}
+    >
       {icon && iconPost === "left" && icon}
-      {title && <span className="leading-2">{title}</span>}
+      {title && <span>{title}</span>}
       {icon && iconPost === "right" && icon}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
